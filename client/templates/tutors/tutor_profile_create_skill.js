ed91734f494751ffffe31d2571a86810d186ac1b
J T <wytanj@gmail.com>
2016-01-10 20:27:21 +0800

    Tweaked tutor skill creation form
    #TIL: push() did not work on array somehow, so addtoset had to be used.

--- client/templates/tutors/tutor_profile_create_skill.js
+++ client/templates/tutors/tutor_profile_create_skill.js
@@ -1,34 +1,45 @@
-Template.tutorSkillCreate.onCreated(function() {
-  Session.set('tutorSkillCreateErrors', {});
+Template.tutorProfileCreateSkill.onCreated(function() {
+  Session.set('tutorProfileCreateSkillErrors', {});
 });
 
-Template.tutorSkillCreate.helpers({
+Template.tutorProfileCreateSkill.helpers({
   errorMessage: function(field) {
-    return Session.get('tutorSkillCreateErrors')[field];
+    return Session.get('tutorProfileCreateSkillErrors')[field];
   },
   errorClass: function (field) {
-    return !!Session.get('tutorSkillCreateErrors')[field] ? 'has-error' : '';
+    return !!Session.get('tutorProfileCreateSkillErrors')[field] ? 'has-error' : '';
   }
 });
 
-Template.tutorSkillCreate.events({
+Template.tutorProfileCreateSkill.events({
   'submit form': function(e) {
     e.preventDefault();
 
     const currentUserId = Meteor.userId();
+    console.log(currentUserId)
 
     var skillProperties = { // takes in skillname, proficiency and price from UI
       skillname: $(e.target).find('[name=skillname]').val(),
       proficiency: $(e.target).find('[name=proficiency]').val(),
-      price: $(e.target).find('[name=price]').val()
+      price: $(e.target).find('[name=price]').val(),
+      tools: $(e.target).find('[name=tools]').val(),
+      projects: $(e.target).find('[name=projects]').val(),
+      philosophy: $(e.target).find('[name=philosophy]').val()
     }
-    var currentUser = Tutors.Collection.find({tutorProfileId: currentUserId});
+    console.log(skillProperties)
+    var currentUser = Tutors.findOne({tutorProfileId: currentUserId});
+    console.log(currentUser)
+    Tutors.update({_id: currentUser._id },{ $addToSet: {expertise: skillProperties } })
+    console.log(currentUser.expertise)
 
     var skillExists = currentUser.expertise.forEach((obj) => {
       return obj.skillname == skillProperties.skillname
     })
 
     if (skillExists) { return "Skill already exists" }
-    else {Tutors.find({tutorProfileId: currentUserId}).expertise.push(skillProperties);}
+    else {
+    // currentUser.expertise.push('skillProperties');
+    window.alert('You added a new skill!')
+    }
   }
 });
